0196bbceef2462211aff4e8fcd8a9a05b99323d6
Rachel Novoselac <r.novoselac@gmail.com>
2024-04-13 23:47:10 -0400

    fixed spacing issue

--- src/board/Board.jsx
+++ src/board/Board.jsx
@@ -2,7 +2,7 @@ export default function Board({canvasRef}) {
 
     return (
       <div className='basis-1/2 flex flex-col bg-diagonal-stripes-dark border-dark-blue border-4 mr-2'>
-        <canvas ref={canvasRef} id="boardCanvas" width="100" height="200"></canvas>
+        <canvas ref={canvasRef} id="boardCanvas" width="101" height="200"></canvas>
       </div>
     )
   };
